6fd0f280414948a48b1231cff6bcc9ba25f0d002
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-09-27 08:16:47 +0200

    TECNO-BG6: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: tecno Model: TECNO BG6 Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_TECNO-BG6.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_TECNO-BG6-user \
+    omni_TECNO-BG6-userdebug \
+    omni_TECNO-BG6-eng
---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for TECNO TECNO BG6 (TECNO-BG6)
+
+```
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
