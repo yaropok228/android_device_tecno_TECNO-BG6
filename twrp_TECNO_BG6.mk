7f892d29996be8c4cdb2a1e7a434a071d23487e5
yaropok228 <143721263+yaropok228@users.noreply.github.com>
2026-07-19 21:26:53 +0700

    Update twrp_TECNO_BG6.mk

--- twrp_TECNO_BG6.mk
+++ twrp_TECNO_BG6.mk
@@ -5,14 +5,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-
-# Inherit from TECNO-BG6 device
 $(call inherit-product, device/tecno/TECNO-BG6/device.mk)
 
 PRODUCT_NAME := twrp_TECNO_BG6
