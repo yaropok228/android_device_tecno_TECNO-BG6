220d0f54faf589cda2d6e519a1f40b4f9a1bf07d
yaropok228 <143721263+yaropok228@users.noreply.github.com>
2026-07-19 21:42:56 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/tecno/TECNO-BG6
+DEVICE_PATH := device/tecno/TECNO_BG6
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
