3b8f3753441719379ef811bc2ecd9d761ee25acf
yaropok228 <143721263+yaropok228@users.noreply.github.com>
2026-07-19 21:44:30 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_TECNO-BG6-user
-add_lunch_combo omni_TECNO-BG6-userdebug
-add_lunch_combo omni_TECNO-BG6-eng
+#add_lunch_combo omni_TECNO-BG6-user
+#add_lunch_combo omni_TECNO-BG6-userdebug
+#add_lunch_combo omni_TECNO-BG6-eng
