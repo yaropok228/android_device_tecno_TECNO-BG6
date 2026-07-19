18122678f25732674ccc3ea9a75bdf68aac91ab1
yaropok228 <143721263+yaropok228@users.noreply.github.com>
2026-07-19 20:22:18 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -31,11 +31,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     bootctrl.ums9230
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.ums9230 \
-    libgptutils \
-    libz \
-    libcutils
+#PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+#    bootctrl.ums9230 \
+#    libgptutils \
+#    libz \
+#    libcutils
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
